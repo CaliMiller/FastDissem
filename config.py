29734859cf41c68cf2abff20aed35323b61fde21
Cali Miller <raditude1@gmail.com>
2025-03-31 00:13:12 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Placeholder for user settings
+user_settings = {
+    "bluesky_api_key": "",
+    "facebook_api_key": "",
+    "twitter_api_key": "",
+    "youtube_api_key": "",
+    "youtube_channel_id": "",
+    "nws_location": ""
+}
+
+latest_youtube_video = None
